25f57a29993022f2c8fd3a25cf573ee8d2c7e915
l1420031550 <l1420031550@outlook.com>
2020-10-28 13:51:51 +0800

    1

---
+++ DataMemberExample/DataMemberExample/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataMemberExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> stulist  = new List<Student>();
+            for (int i = 0; i < 100; i++)
+            {
+                Student stu = new Student();
+                stu.Age = 24;
+                stu.Score = i;
+
+                stulist.Add(stu);
+
+            }
+            int totalAge=0;
+            int totalScore=0;
+            foreach (var stu in stulist)
+            {
+                totalAge += stu.Age;
+                totalScore += stu.Score;
+            }
+            Student.AverageAge = totalAge / 100;
+            Student.AverareScore = totalScore / 100;
+            Student.ReportAverageAge();
+            Student.ReporAverareScore();
+
+            Student.reportAmount();
+            Console.ReadLine();
+            
+        }
+    }
+    class Student
+    {
+        public int  Age;
+        public int  Score;
+
+        public static int amount;
+        public static int AverageAge;
+        public static int AverareScore;
+
+        public Student()
+        {
+            Student.amount++;
+
+        }
+        public static void reportAmount()
+        {
+            Console.WriteLine(Student .amount);
+            
+        }
+        public static void ReportAverageAge()
+        {
+            Console.WriteLine(AverageAge);
+        
+        }
+        public static void ReporAverareScore()
+        {
+            Console.WriteLine(AverareScore);
+         
+        }
+    }
+}
---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            wrapFactory wrapFactory = new wrapFactory();
+            ProductFactory productFactory = new ProductFactory();
+
+            Func<Product> func = new Func<Product>(productFactory.MakePizza);
+            Box box = wrapFactory.WrapProduct(func);
+            Console.WriteLine(box.product.Name);
+            Console.ReadLine();
+        }
+    }
+    class Product     //产品类 有name属性
+    {
+        public string Name { get; set; }
+    }
+    class Box          //包装箱子 ，可以包装不同产品
+    {
+        public Product product { get; set; }
+    }
+    class wrapFactory      //包装工厂
+    {
+        public Box WrapProduct(Func<Product> getProduct)
+        {
+            Box box = new Box();
+            Product product = getProduct.Invoke();
+            box.product = product;
+            return box; 
+        }
+    }
+    class ProductFactory //产品工厂
+    {
+        public Product MakePizza()
+        {
+            Product product = new Product();
+            product.Name = "This is pizza";
+            return product;
+        }
+        public Product MakeKole()
+        {
+            Product product = new Product();
+            product.Name = "This is kole";
+            return product;
+
+        }
+    }
+}
